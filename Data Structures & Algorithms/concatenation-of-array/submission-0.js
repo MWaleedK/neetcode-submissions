c6e405d27fb57cc5ea686531a47465779c37f728
VulcanBin <58626386+MWaleedK@users.noreply.github.com>
2026-04-20 11:56:26 +0500

    Add: concatenation-of-array - submission-0

---
+++ Data Structures & Algorithms/concatenation-of-array/submission-0.js
@@ -0,0 +1,10 @@
+class Solution {
+    /**
+     * @param {number[]} nums
+     * @return {number[]}
+     */
+    getConcatenation(nums) {
+        let concatednatedArray = nums.concat(nums);
+        return concatednatedArray;
+    }
+}
